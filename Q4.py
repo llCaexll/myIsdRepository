14b037c85ba0d939bf81b9a996f229487309e602
llCaexll <21294834@student.uwl.ac.uk>
2016-01-09 15:56:44 +0000

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first Number "))
+b=int(input("Enter Second Number "))
+c=a+b
+print(a,"+",b,"=",c)
+c=a-b
+print(a,"-",b,"=",c)
+c=a*b
+print(a,"*",b,"=",c)
+c=a/b
+print(a,"/",b,"=",c)
